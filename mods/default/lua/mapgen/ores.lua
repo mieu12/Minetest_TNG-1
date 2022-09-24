9eacba04b8854e3c7e09e265b394327a57d3c6e1
mieu12 <106581592+mieu12@users.noreply.github.com>
2022-09-24 17:58:26 +1000

    Add mithril generation.

--- mods/default/lua/mapgen/ores.lua
+++ mods/default/lua/mapgen/ores.lua
@@ -277,3 +277,15 @@ core.register_ore({
 	y_min          = -31000,
 	y_max          = -1024,
 })
+-- Mithril ore
+
+	core.register_ore({
+		ore_type       = "scatter",
+		ore            = "default:" .. stone .. "_with_mithril",
+		wherein        = "default:" .. stone,
+		clust_scarcity = 19 * 19 * 19,
+		clust_num_ores = 4,
+		clust_size     = 3,
+		y_min          = -31000,
+		y_max          = -512,
+	})
